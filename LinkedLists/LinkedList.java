fba68342d8620f348e0b111b89f6d0ff74791e11
v-sonawane <vsona200900@gmail.com>
2022-05-24 17:54:16 +0530

    Adding LinkedList in JAVA

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -1,8 +1,9 @@
 public class LinkedList{
-    
+
     Node head;
 
-    static class Node{
+    class Node{
+        
         int data;
         Node next;
 
@@ -12,42 +13,46 @@ public class LinkedList{
         }
     }
 
-    public LinkedList insert(LinkedList list, int data){
-        Node new_node=new Node(data);
+    LinkedList insert(LinkedList list, int value){
+        
+        Node new_node = new Node(value);
         new_node.next=null;
 
         if(list.head==null){
             list.head=new_node;
         }
         else{
+
             Node last=list.head;
+
             while(last.next!=null){
                 last=last.next;
             }
+
             last.next=new_node;
         }
+
         return list;
     }
 
     void printList(LinkedList list){
         Node current_node=list.head;
-
-        System.out.println("LinkedList: ");
-
-        while(current_node!=null){
-            System.out.print(current_node.data+" ");
-            current_node=current_node.next;
+        if(current_node==null){
+            System.out.println("No items are present in the list");
+        }
+        else{
+            System.out.println("LinkedList: ");
+            while(current_node!=null){
+                System.out.print(current_node.data+" ");
+                current_node=current_node.next;
+            }
         }
     }
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         LinkedList list=new LinkedList();
-
         list.insert(list, 1);
-        list.insert(list, 2);
-        list.insert(list, 3);
-
+        list.insert(list,2);
         list.printList(list);
     }
 }
-
